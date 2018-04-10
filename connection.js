542b9ebb6e7e39535982a92073f22ae3bd5e4fb6
ardhiesta <ardhi.mail@gmail.com>
2018-04-10 10:05:31 +0700

    change user to username

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('./config/config')[env];
 const connection = mysql.createConnection({
   host: config.database.host,
-  user: config.database.user,
+  user: config.database.username,
   password: config.database.password,
   database: config.database.db_name
 });
